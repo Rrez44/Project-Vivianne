2c6c52b86391148ca8a112a79360c7ecbaa07330
AlbinArifaj <albiniarifaj@gmail.com>
2024-04-28 16:27:20 +0200

    Added MVN

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
-//module com.example.demo1 {
-//    requires javafx.controls;
-//    requires javafx.fxml;
+module com.example.demo1 {
+    requires javafx.controls;
+    requires javafx.fxml;
 
-
-//    opens com.example.demo1 to javafx.fxml;
-//    exports com.example.demo1;
-//}
+    opens com.example.demo1 to javafx.fxml;
+    exports com.example.demo1;
+}
