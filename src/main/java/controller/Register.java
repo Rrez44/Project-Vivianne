d3639f4b5c176b236743681b95ff3e9c5d6fd920
ReartKr <147925754+ReartKrasniqi20@users.noreply.github.com>
2024-05-02 16:08:50 -0700

    Add register controller

---
+++ src/main/java/controller/Register.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Register {
+
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private DatePicker dateBirthday;
+    
+    @FXML
+    private PasswordField pwdPassword;
+    
+    @FXML
+    private PasswordField pwdConfirmPassword;
+
+
+    public void handleAddBus(ActionEvent event) {
+    }
+
+    public void handleSuspend(ActionEvent event) {
+    }
+}
