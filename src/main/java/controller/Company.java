8af95d88a0946ce6c13f9f322ff4c352269a81cb
Rrezon <rrezon.beqiri@student.uni-pr.edu>
2024-05-05 15:39:47 +0200

    Added most the functionality for companyView

--- src/main/java/controller/Company.java
+++ src/main/java/controller/Company.java
@@ -1,10 +1,14 @@
 package controller;
 
+import ENUMS.ActivityStatus;
+import ENUMS.AreaCode;
 import app.Navigator;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import repository.CompanyRepository;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -30,15 +34,27 @@ public class Company extends BGmain implements Initializable {
     
     @FXML
     private MenuButton mbtnAreaCode;
+    @FXML
+    private Button btnSuspend;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnEdit;
 
     private static model.Company company;
+
+    private int editing = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         txtCompanyName.setText(company.getCompanyName());
         txtAreaDescription.setText(company.getDescription());
         labelStatus.setText(company.getCompanyStatus().name());
-        labelAreaCode.setText(company.getAreaCode().toString());
+        labelAreaCode.setText(company.getAreaCode().name());
+        txtCompanyName.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+        txtAreaDescription.setStyle("-fx-control-inner-background:  #2B2D42; -fx-text-fill: white;");
+        mbtnAreaCode.setText(company.getAreaCode().name());
+        updateStatusManagerView();
     }
 
 
@@ -48,19 +64,66 @@ public class Company extends BGmain implements Initializable {
     public void handleAddBus(ActionEvent actionEvent) {
     }
 
-    public void handleSuspend(ActionEvent actionEvent) {
-    }
+
 
     public void handleAreaCode(ActionEvent actionEvent) {
+        MenuItem item =(MenuItem) actionEvent.getSource();
+        mbtnAreaCode.setText(item.getText());
+        labelAreaCode.setText(mbtnAreaCode.getText());
     }
 
     public void handleFilter(ActionEvent actionEvent) {
     }
 
     public void handleEdit(ActionEvent actionEvent) {
+        switch (editing) {
+            case 0:
+                txtCompanyName.setEditable(true);
+                txtAreaDescription.setEditable(true);
+                txtCompanyName.setStyle("-fx-background-color: white; -fx-text-fill: black;");
+                txtAreaDescription.setStyle("-fx-background-color: white; -fx-text-fill: black;");
+                labelAreaCode.setVisible(false);
+                mbtnAreaCode.setVisible(true);
+                btnSave.setDisable(false);
+                editing = 1;
+                btnEdit.setText("Cancel");
+                break;
+            case 1:
+                txtCompanyName.setEditable(false);
+                txtAreaDescription.setEditable(false);
+                txtCompanyName.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
+                txtAreaDescription.setStyle("-fx-control-inner-background:  #2B2D42; -fx-text-fill: white;");
+                labelAreaCode.setVisible(true);
+                mbtnAreaCode.setVisible(false);
+                btnSave.setDisable(true);
+                btnEdit.setText("Edit");
+                editing = 0;
+        }
     }
 
+
+
     public void handleSave(ActionEvent actionEvent) {
+        if(txtCompanyName.getText().trim().equals(company.getCompanyName()) && txtAreaDescription.getText().trim().equals(company.getDescription()) && labelAreaCode.getText().trim().equals(company.getAreaCode().name())){
+            showError("Nothing has changed", "nothing to save here");
+        }
+        if (txtCompanyName.getText().trim().isEmpty()){
+            showError("Invalid company name", "The company name cannot be empty");
+        }
+        if (txtAreaDescription.getText().trim().isEmpty()){
+            showError("Invalid company description", "The company description cannot be empty");
+        }
+        company.setCompanyName(txtCompanyName.getText().trim());
+        company.setDescription(txtAreaDescription.getText().trim());
+        company.setAreaCode(AreaCode.valueOf(mbtnAreaCode.getText()));
+        try {
+            if(CompanyRepository.updateCompany(company)){
+                showConfirmation("Company changes saved", "The process was a success");
+                handleEdit(new ActionEvent());
+            }
+        }catch (RuntimeException re){
+            showError("Error updating company", re.getMessage());
+        }
     }
 
     public static void passCompany(model.Company cmp){
@@ -68,4 +131,31 @@ public class Company extends BGmain implements Initializable {
         company = cmp;
     }
 
+    public void handleStatusManager(ActionEvent actionEvent) {
+        switch (company.getCompanyStatus()){
+            case ACTIVE :
+                            company.setCompanyStatus(ActivityStatus.SUSPENDED);
+                            labelStatus.setText("Suspended");
+                            updateStatusManagerView();
+                            CompanyRepository.updateCompany(company);
+                            showConfirmation("Coompany Suspended", "The activities of this company have been suspended");
+
+                            break;
+            case SUSPENDED:
+                            company.setCompanyStatus(ActivityStatus.ACTIVE);
+                            labelStatus.setText("Active");
+                            updateStatusManagerView();
+                            CompanyRepository.updateCompany(company);
+                            showConfirmation("Company Activated", "The activities of this company have been activated");
+                            break;
+        }
+
+    }
+    private void updateStatusManagerView(){
+        // this method updates the button so it corresponds to the activity status
+        switch (company.getCompanyStatus()){
+            case ACTIVE : btnSuspend.setText("Suspend"); btnSuspend.setStyle("-fx-background-color:  #ef233c; -fx-font-weight: bold" ); break;
+            case SUSPENDED: btnSuspend.setText("Activate"); btnSuspend.setStyle("-fx-background-color:   #1DB954; -fx-font-weight: bold"); break;
+        }
+    }
 }
